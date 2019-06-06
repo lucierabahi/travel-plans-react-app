67e10c5cf8ac0ab70b41036c357df1196cb10bf6
Lucie Rabahi <lucierabahi@protonmail.com>
2019-06-06 11:10:02 +0200

    Updates

---
+++ precache-manifest.59eb6ad32c3991cdbdc33105e5ce8e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3baf10461b981b587598f2cd0f57c99",
+    "url": "/travel-plans-react-app/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/travel-plans-react-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "691a7dfc53e004e2058c",
+    "url": "/travel-plans-react-app/static/css/main.a8cb2089.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/travel-plans-react-app/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "691a7dfc53e004e2058c",
+    "url": "/travel-plans-react-app/static/js/main.6b45eee5.chunk.js"
+  },
+  {
+    "revision": "0f7de60b57e20f73a660",
+    "url": "/travel-plans-react-app/static/js/runtime~main.3b069595.js"
+  },
+  {
+    "revision": "b381f6001c4c2c3974c753f2bf5e4ae8",
+    "url": "/travel-plans-react-app/static/media/Aluria.b381f600.woff"
+  }
+]);
